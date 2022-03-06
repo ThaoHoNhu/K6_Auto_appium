4fdf270f287bfb1b3ace9525ee489aaf8a740ebe
Nhuthao Ho <hothao@Nhuthaos-MacBook-Air.local>
2022-03-06 14:48:24 +0700

    lab4

---
+++ src/lesson_05/lab_5_1.java
@@ -0,0 +1,51 @@
+package lesson_05;
+import java.util.*;
+
+
+public class lab_5_1 {
+    public static void main(String[] args) {
+
+        boolean isContinuing = true;
+        Scanner scanner = new Scanner(System.in);
+        HashMap<Integer, String> mapStudent = new HashMap<>();
+        while(isContinuing){
+            System.out.println("=====MENU======");
+            System.out.println("1. Input student info");
+            System.out.println("2. Find Student by ID");
+            System.out.println("0. Exit!");
+            System.out.println("Input your option");
+            int userInput = scanner.nextInt();
+            if(userInput == 0){
+                System.out.println("Exit");
+                isContinuing = false;
+            }else {
+                if (userInput == 1){
+                    //input Student Info
+                    System.out.println("Option 01");
+                    System.out.print("Enter Student ID:");
+                    int idNumber = scanner.nextInt();
+                    System.out.print("Enter Studen name:");
+                    Scanner myObj = new Scanner(System.in);
+                    String name = myObj.nextLine();
+                    mapStudent.put(idNumber,name);
+
+                }
+                if (userInput == 2){
+                    //find id,
+                    System.out.println("Option 02");
+                    System.out.print("Enter ID number to find:");
+                    int idNumberFind = scanner.nextInt();
+                    if (mapStudent.containsKey(idNumberFind)){
+                        System.out.println("The student with ID " + idNumberFind +
+                                " has name " + mapStudent.get(idNumberFind));
+                    }
+                    else{
+                        System.out.println("Not found student with ID input");
+                    }
+
+                }
+            }
+
+        }
+    }
+}
